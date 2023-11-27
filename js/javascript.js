54924e9807f9118c6086f061814c2f083de0e7e6
Benjamin Nieto <benjaminnieto98@gmail.com>
2023-11-27 13:33:31 -0300

    posiciona sec2 y duende verde

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,6 @@
 "use strict";
 let header = document.querySelector("header");
 window.addEventListener("scroll", () => {
-    console.log(window.scrollY)
     if (window.scrollY > 0) {
         header.classList.add("scrolleando")
     } else if (window.scrollY == 0) {
